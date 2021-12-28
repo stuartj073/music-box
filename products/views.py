a29813937bc3c9ad62d86ab66054a8d17552ace7
stuartj073 <stuartj073@gmail.com>
2021-12-28 17:25:29 +0000

    Add links for bootstrap and font awesome to base.html

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_objects_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product, Category
 
 # Create your views here.
