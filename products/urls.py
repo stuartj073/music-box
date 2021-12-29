9701f105e7286b1461dab5bfe65d95979d99847f
stuartj073 <stuartj073@gmail.com>
2021-12-29 13:18:01 +0000

    Add product detail view to products app

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.products, name='products')
+    path('', views.products, name='products'),
+    path('product_details/', views.product_detail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -12,3 +12,13 @@ def products(request):
         'categories': categories,
     }
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_details.html', context)
