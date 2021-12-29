53054305b6053e210c69be92731d496b42bc9531
stuartj073 <stuartj073@gmail.com>
2021-12-29 17:58:18 +0000

    Add search-bar div to wrap around search form

--- home/views.py
+++ home/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
+from django.db.models import Q
+from products.models import Product, Category
 
 # Create your views here.
 
 def index(request):
     """ Index page request. """
-    return render(request, 'home/index.html')
+    """ Search bar request. """
+    products = Product.objects.all()
+    categories = Category.objects.all()
+
+    if request.method == "GET":
+        if 'search' in request.method:
+            query = request.GET['search']
+            if not query:
+                print("You didn't specify your search")
+                return redirect(reverse('home'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+    context = {
+        'products': products,
+        'categories': categories,
+    }
+
+    return render(request, 'home/index.html', context)
+
+
+
