39d06240b721729952d90151abdbb68e378254fb
stuartj073 <stuartj073@gmail.com>
2021-12-30 11:30:48 +0000

    Add basic navbar to site along with javascript links for dropdown element to function

--- products/models.py
+++ products/models.py
@@ -31,8 +31,9 @@ class Product(models.Model):
     description = models.TextField()
     condition = models.TextField()
     date = models.DateField(null=True, blank=True, default=False)
-    rating = models.DecimalField(max_digits=5, decimal_places=2)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    rating = models.DecimalField(max_digits=5, decimal_places=2,
+                                 null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
     warranty = models.BooleanField(null=True, blank=True)
