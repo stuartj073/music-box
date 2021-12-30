9f2047666b1ae3d601f69c751be2d6f077afced3
stuartj073 <stuartj073@gmail.com>
2021-12-30 14:58:33 +0000

    Add log in and log out links to base html

--- music_box/settings.py
+++ music_box/settings.py
@@ -150,7 +150,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # CONNECTION FOR STATIC FILE TO BASE.HTML
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+#  MEDIA FILE CONNECTIONS
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
