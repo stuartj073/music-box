535e5081abaf61307aa487ceb5346e01ebe2f685
stuartj073 <stuartj073@gmail.com>
2022-01-08 15:47:05 +0000

    Add view for product review

--- product_review/views.py
+++ product_review/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from products.model import Product
 
 # Create your views here.
+
+def product_review(request, product_id):
+    """ Return the product review page. """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return redirect(request, 'product_review.html', context)
