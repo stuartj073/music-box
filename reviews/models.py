1503afed83719a2c62645d9d03693d87bfbe3d55
stuartj073 <stuartj073@gmail.com>
2022-01-09 16:19:50 +0000

    Add product reviews model to reviews app

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from products.models import Product, Category
 
 # Create your models here.
+
+
+class ProductReview(models.Model):
+    """ Allow user to create review based on specific product."""
+    name = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+    category = models.ForeignKey('products.Category', on_delete=models.CASCADE)
+    description = models.TextField()
+    date = models.DateField(auto_now_add=True, blank=False,
+                            null=False)
+
+    def __str__(self):
+        return self.name
