6353279c466873d6c8545fb48ccdd091bab9a133
stuartj073 <stuartj073@gmail.com>
2022-01-10 18:21:58 +0000

    Add blog details url

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('blog', views.blog, name='blog'),
     path('add', views.add_blog, name='add_blog'),
+    path('blog_details/<int:blog_id>', views.blog_details, name='blog_details'),
 ]
