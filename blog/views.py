cc7821da489295816e554c76cb3586a0776fd12a
stuartj073 <stuartj073@gmail.com>
2022-01-10 12:11:27 +0000

    Add blog view to blog app

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
+
+def blog(request):
+    """ Display all blog posts by users. """
+
+    blog = Blog.object.all()
+
+    context = {
+        'blog': blog,
+    }
+
+    return render(request, "blog.html", context)
