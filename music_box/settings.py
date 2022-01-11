824fa3b24fd43358408c554523b86893b3c9b2de
stuartj073 <stuartj073@gmail.com>
2022-01-11 18:57:33 +0000

    Removed secret key and set debug

--- music_box/settings.py
+++ music_box/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^v%#7xjcr$w=skr*(s*vj!ynyc7m)^nlr+ai6efm153^g-jweh'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['music-box-stuartj-herokuapp.com', 'localhost']
 
