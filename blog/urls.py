55427776be619fe4744e3de45b1d480c0438df13
stuartj073 <stuartj073@gmail.com>
2022-01-11 10:30:34 +0000

    Add update blog url to urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('add', views.add_blog, name='add_blog'),
     path('blog_details/<int:blog_id>', views.blog_details, name='blog_details'),
     path('delete/<int:blog_id>', views.delete_blog, name='delete_blog'),
+    path('update/<int:blog_id>', views.update_blog, name='update_blog'),
 ]
