329a30015981e4df56162bb1125d7a923c1024dc
stuartj073 <stuartj073@gmail.com>
2022-01-13 09:35:20 +0000

    Disable collectstatic and push changes

--- music_box/settings.py
+++ music_box/settings.py
@@ -186,7 +186,7 @@ if 'USE_AWS' in os.environ:
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
-    # Overide static and media urls
+    # Overide the static and media urls
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
