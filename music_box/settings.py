483ceddf916ea8de3985ed1d277b2893b9ee0aa1
stuartj073 <stuartj073@gmail.com>
2022-01-20 16:59:45 +0000

    Remove old AWS configuration keys and replace with newly created one's

--- music_box/settings.py
+++ music_box/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.setdefault("DEBUG")
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = ['music-box-stuartj.herokuapp.com', 'localhost']
 
