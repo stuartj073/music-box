70b7e4a46efbee33d53ba3d2140fac25bcf18c8b
stuartj073 <stuartj073@gmail.com>
2022-01-24 14:40:28 +0000

    Create add to basket view in basket app

--- basket/urls.py
+++ basket/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.basket, name='basket'),
+    path('add_to_basket/<item_id>', views.add_to_basket, name='add_to_basket'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from decimal import Decimal
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
 
@@ -7,4 +7,28 @@ from decimal import Decimal
 def basket(request):
     """ Show basket page for user. """
 
-    return render(request, "basket.html")
+    context = {
+        'basket': basket,
+    }
+
+    return render(request, "basket.html", context)
+
+
+def add_to_basket(request, item_id):
+    """ Add product to basket. """
+    quantity = int(request.POST.get("amount"))
+    redirect_url = request.POST.get("redirect_url")
+    basket = request.session.get("basket", {})
+
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
+        messages.success(request, "Item added")
+    else:
+        basket[item_id] = quantity
+        messages.success(request, "Item added")
+
+    request.session['basket'] = basket
+    print(request.session['basket'])
+    print("YEHO")
+
+    return redirect(redirect_url)
