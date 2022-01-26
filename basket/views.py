125d7f5503b9337a0bbf5b815ad25ff85db883ec
stuartj073 <stuartj073@gmail.com>
2022-01-26 17:32:26 +0000

    Add edit basket view and update quantity to amount

--- basket/views.py
+++ basket/views.py
@@ -27,42 +27,70 @@ def add_to_basket(request, item_id):
     if size:
         if item_id in list(basket.keys()):
             if size in basket[item_id]['items_by_size'].keys():
-                basket[item_id]['items_by_size'][size] += quantity
+                basket[item_id]['items_by_size'][siamount
                 messages.success(request,
                                  (f'Updated size {size.upper()} '
-                                  f'{product.name} quantity to '
+                                  f'{product.name} amount to '
                                   f'{basket[item_id]["items_by_size"][size]}'))
             else:
-                basket[item_id]['items_by_size'][size] = quantity
+                basket[item_id]['items_by_size'][size] = amount
                 messages.success(request,
                                  (f'Added size {size.upper()} '
                                   f'{product.name} to your basket'))
         else:
-            basket[item_id] = {'items_by_size': {size: quantity}}
+            basket[item_id] = {'items_by_size': {size: amount}}
             messages.success(request,
                              (f'Added size {size.upper()} '
                               f'{product.name} to your basket'))
     else:
         if item_id in list(basket.keys()):
-            basket[item_id] += quantity
+            basket[item_id] += amount
             messages.success(request,
                              (f'Added {product.name} '
-                              f'quantity to {basket[item_id]}'))
+                              f'amount to {basket[item_id]}'))
         else:
-            basket[item_id] = quantity
+            basket[item_id] = amount
             messages.success(request, f'{product.name} has been added to your basket.')
 
     request.session['basket'] = basket
     return redirect(redirect_url)
 
 
-# def remove_from_basket(request, item_id):
-#     """ Remove item from basket. """
+def edit_basket(request, item_id):
+    """Edit the amount of any given product in the basket."""
 
-#     product = get_object_or_404(Product, pk=item_id)
-#     basket = request.session.get('basket', {})
+    product = get_object_or_404(Product, pk=item_id)
+    amount = int(request.POST.get('amount'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    basket = request.session.get('basket', {})
 
-#     basket.pop(item_id)
-#     messages.success(request, f'Removed {product.name} from basket.')
+    if size:
+        if amount > 0:
+            basket[item_id]['items_by_size'][size] = amount
+            messages.success(request,
+                             (f'Updated size {size.upper()} '
+                              f'{product.name} amount to '
+                              f'{basket[item_id]["items_by_size"][size]}'))
+        else:
+            del basket[item_id]['items_by_size'][size]
+            if not basket[item_id]['items_by_size']:
+                basket.pop(item_id)
+            messages.success(request,
+                             (f'Adjusted size {size.upper()} '
+                              f'{product.name} from your basket'))
+    else:
+        if amount > 0
+            basket[item_id] = amount
+            messages.success(request,
+                             (f'Updated {product.name} '
+                              f'amount to {basket[item_id]}'))
+        else:
+            basket.pop(item_id)
+            messages.success(request,
+                             (f'Removed {product.name} '
+                              f'from your basket'))
 
-#     return redirect("basket")
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
