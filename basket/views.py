d0382b52f3dfb285c4758d6a88253d8cbc4b7c74
stuartj073 <stuartj073@gmail.com>
2022-01-26 17:25:32 +0000

    Update add to basket function

--- basket/views.py
+++ basket/views.py
@@ -15,21 +15,44 @@ def basket(request):
 
 
 def add_to_basket(request, item_id):
-    """ Add product to basket. """
-    quantity = int(request.POST.get("amount"))
-    redirect_url = request.POST.get("redirect_url")
-    basket = request.session.get("basket", {})
+    """ View to add product and/or sizes to basket. """
 
-    if item_id in list(basket.keys()):
-        basket[item_id] += quantity
-        messages.success(request, "Item added")
+    product = get_object_or_404(Product, pk=item_id)
+    amount = int(request.POST.get('amount'))
+    redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    basket = request.session.get('basket', {basket
+    if size:
+        if item_id in list(basket.keys()):
+            if size in basket[item_id]['items_by_size'].keys():
+                basket[item_id]['items_by_size'][size] += quantity
+                messages.success(request,
+                                 (f'Updated size {size.upper()} '
+                                  f'{product.name} quantity to '
+                                  f'{basket[item_id]["items_by_size"][size]}'))
+            else:
+                basket[item_id]['items_by_size'][size] = quantity
+                messages.success(request,
+                                 (f'Added size {size.upper()} '
+                                  f'{product.name} to your basket'))
+        else:
+            basket[item_id] = {'items_by_size': {size: quantity}}
+            messages.success(request,
+                             (f'Added size {size.upper()} '
+                              f'{product.name} to your basket'))
     else:
-        basket[item_id] = quantity
-        messages.success(request, "Item added")
+        if item_id in list(basket.keys()):
+            basket[item_id] += quantity
+            messages.success(request,
+                             (f'Added {product.name} '
+                              f'quantity to {basket[item_id]}'))
+        else:
+            basket[item_id] = quantity
+            messages.success(request, f'{product.name} has been added to your basket.')
 
     request.session['basket'] = basket
-    print("YEHO")
-
     return redirect(redirect_url)
 
 
