a39754c813d330ee382c653ae1193b21b74e3e5b
stuartj073 <stuartj073@gmail.com>
2022-01-29 12:34:24 +0000

    Create OrderForm in checkout app

---
+++ checkout/forms.py
@@ -0,0 +1,35 @@
+from django import crispy_forms
+from .models import Order
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('first_name', 'surname', 'date', 'email', 'phone_number', 
+                  'street_address1', 'street_address2', 'town_or_city', 
+                  'county', 'country', 'postcode')
+
+    def __init__(self, *args, **kwargs):
+        """
+        Create placeholders for OrderForm fields
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'first_name': 'First Name',
+            'surname': 'Surname',
+            'email': 'Email',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+            'town_or_city': 'Town or City',
+            'county': 'County',
+            'country': 'Country',
+            'postcode': 'Postcode',
+    }
+
+    self.fields['first_name'].widget.attrs['autofocus'] = True
+    for field in self.fields:
+        if fields != 'country':
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
