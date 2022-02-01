dbaec1bd89657d69bc8f8f8a68c9c05f02816910
stuartj073 <stuartj073@gmail.com>
2022-02-01 10:43:56 +0000

    Add UserForm to forms.py

---
+++ profiles/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import Order
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = Users
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Create placeholders for OrderForm fields
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_email': 'Email',
+            'default_phone_number': 'phone_number',
+            'default_street_address1': 'Street Address 1',
+            'default_street_address2': 'Street Address 2',
+            'default_town_or_city': 'Town or City',
+            'default_county': 'County',
+            'default_postcode': 'Postcode',
+        }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
