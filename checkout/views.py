4d783ba87f9030af65abfa9f3dee322c58ff2558
stuartj073 <stuartj073@gmail.com>
2022-02-02 13:55:36 +0000

    Add values for stripe keys in env.py

--- checkout/views.py
+++ checkout/views.py
@@ -10,10 +10,20 @@ import stripe
 # Create your views here.
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     current_basket = basket_contents(request)
     total = current_basket['checkout_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
 
     if not basket:
         messages.error(request, "Sorry, your bag is empty")
