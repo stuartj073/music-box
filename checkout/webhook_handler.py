321f55204334b7935d5865fb8f11f4115685243a
stuartj073 <stuartj073@gmail.com>
2022-02-07 20:41:00 +0000

    Add automatic email confirmation upon checkout completion

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 
 from .models import Order, OrderLineItem
 from products.models import Product
@@ -14,6 +16,23 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
+
+    def _send_confirmation_email(self, order):
+        """ Send the user a confirmation email """
+        customer_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+        
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [customer_email]
+        )  
 
     def handle_event(self, event):
         """
@@ -66,6 +85,7 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(5)
         if order_exists:
+            self._send_confirmation_email(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
                 status=200)
@@ -107,9 +127,10 @@ class StripeWH_Handler:
             except Exception as e:
                 if order:
                     order.delete()
-                return HttpRespsone(
+                return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
+        self._send_confirmation_email(order)         
         return HttpResponse(
             content=f'Webhook received:{event["type"]} | SUCCESS: Created webhook',
             status=200)
--- music_box/settings.py
+++ music_box/settings.py
@@ -188,6 +188,7 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
+DEFAULT_FROM_EMAIL = 'james@musicbox.ie'
 
 
 # Static files (CSS, JavaScript, Images)
