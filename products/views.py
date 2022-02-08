2141e9368b173f2d8c901f04f167fef5a942b55b
stuartj073 <stuartj073@gmail.com>
2022-02-08 10:22:12 +0000

    Change request.method in edit product view

--- products/views.py
+++ products/views.py
@@ -112,7 +112,7 @@ def edit_product(request, product_id):
 
     product = get_object_or_404(Product, pk=product_id)
 
-    if request.method == "GET":
+    if request.method == "POST":
         form = ProductForm(request.POST, request.FILES, instance=product)
         if form.is_valid():
             form.save()
