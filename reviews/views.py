c985f75b6b54f744e7b00f95f3fde090933b04c7
stuartj073 <stuartj073@gmail.com>
2022-02-08 12:38:55 +0000

    Include product in context for product reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,33 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from products.models import Product
+from .models import ProductReview
 from django.contrib import messages
 from .forms import ProductReviewForm
 
 # Create your views here.
 
 
-def product_review(request, product_id):
+def product_reviews(request, product_id):
     """ Show product review form. """
 
-    product = get_object_or_404(Product, pk=product_id)
-    
-    if request.method == "POST":
-        form = ProductReviewForm(request.POST, request.FILES)
-        if form.is_valid():
-            review = form.save()
-            messages.success(request, "Review saved")
-            print("YAP")
-            return redirect(reverse('product_review', args=[review.id]))
-        else:
-            print("Form is invalid")
-            return redirect(reverse("product_review"))
+    reviews = ProductReview.objects.all()
 
-    else:
-        form = ProductReviewForm()
-        form.save()
-
-    context ={
-        'product': product,
+    context = {
+        'reviews': reviews,
     }
 
     return render(request, 'product_reviews.html', context)
@@ -47,7 +33,7 @@ def add_review(request, product_id):
             form.save()
         else:
             messages.error(request, "Form invalid, please try again.")
-            return render(reverse("add_product", args=[product.id]))
+            return redirect(reverse('product_details', args=[product.id]))
     else:
         form = ProductReviewForm()
 
@@ -55,6 +41,7 @@ def add_review(request, product_id):
 
     context = {
         'form': form,
+        'product': product,
     }
 
     return render(request, template, context)
