d266ab2d0ff3f61bec16908316a35285c43bc5ea
stuartj073 <stuartj073@gmail.com>
2022-02-17 20:58:54 +0000

    Exclude sku from products form

--- products/forms.py
+++ products/forms.py
@@ -7,7 +7,7 @@ class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        fields = '__all__'
+        exclude = ('sku',)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
