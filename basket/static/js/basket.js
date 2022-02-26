63202f6820812e34667f33b616c5b19efd286e05
stuartj073 <stuartj073@gmail.com>
2022-02-26 19:37:06 +0000

    Increase margin of add remove buttons in basket

--- basket/static/js/basket.js
+++ basket/static/js/basket.js
@@ -4,18 +4,17 @@
 $('.update-link').click(function (e) {
     var form = $(this).prev('.update-form');
     form.submit();
-    console.log("HI")
 });
 
 // Remove relevant item and reload on click
 $('.remove-item').click(function (e) {
     var csrfToken = "{{ csrf_token }}";
     var itemId = $(this).attr('id').split('remove_')[1];
-    var size = $(this).data('size');
+    var size = $(this).data('product_size');
     var url = `/basket/remove/${itemId}`;
     var data = {
         'csrfmiddlewaretoken': csrfToken,
-        'size': size
+        'product_size': size
     };
 
     $.post(url, data)
