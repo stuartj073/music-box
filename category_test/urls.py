a20201eb8fceb4e96e71176c69f09f3454b5ca9c
stuartj073 <stuartj073@gmail.com>
2022-02-26 23:22:26 +0000

    Remove category test url

--- category_test/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.category, name='category'),
-    path('category_details/<int:category_id>', views.category_details, name='category_details'),
-    path('records', views.records, name='records')
-]
